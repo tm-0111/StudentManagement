b10107a2d42ad439bdc21bdc3bc881510d776602
tm-0111 <l2502893856as@gmail.com>
2025-06-25 17:49:42 +0900

    kadai

--- src/test/java/raisetech/student/management/Repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/student/management/Repository/StudentRepositoryTest.java
@@ -2,12 +2,44 @@ package raisetech.student.management.Repository;
 
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import raisetech.student.management.date.Student;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 @MybatisTest
 class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository sut;
+
 @Test
-    void test(){}
+    void 受講生の全件検索が行えること(){
+    List<Student> actual = sut.search();
+    assertThat(actual.size()).isEqualTo(5);
+
+}
+@Test
+    void 受講生の登録が行えること(){
+    Student student = new Student();
+    student.setName("山田太郎");
+    student.setKanaName("ヤマダタロウ");
+    student.setNickname("タロー");
+    student.setEmail("taro@example.com");
+    student.setArea("東京");
+    student.setAge(25);
+    student.setSex("男性");
+    student.setRemark("");
+    student.setDeleted(false);
+
+    sut.registerStudent(student);
+
+    List<Student> actual = sut.search();
+
+    assertThat(actual.size()).isEqualTo(6);
+}
 
 }
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,19 +1,18 @@
-   CREATE TABLE IF NOT EXISTS students
-   (id VARCHAR(36) PRIMARY KEY,
+   CREATE TABLE IF NOT EXISTS students(
+   id VARCHAR(36) PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     kana_name VARCHAR(50) NOT NULL,
     nickname VARCHAR(50),
     email VARCHAR(50) NOT NULL,
     area VARCHAR(50),
     age INT,
-    sex VARCHAR(10)
-    remake TEXT,
-    is_deleted boolean
+    sex VARCHAR(10),
+    remark TEXT,
+    is_deleted BOOLEAN
     );
 
-CREATE TABLE IF NOT EXISTS students_courses
-(
-    id VARCHAR(36) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS students_courses(
+    id VARCHAR(36) PRIMARY KEY AUTO_INCREMENT,
     student_id VARCHAR(36) NOT NULL,
     course_name VARCHAR(50) NOT NULL,
     course_start_at TIMESTAMP,
