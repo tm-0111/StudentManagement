decd1f4b3c7c6723b49c6d4b1d20d20401fe903a
tm-0111 <l2502893856as@gmail.com>
2025-07-10 15:00:19 +0900

    課題30　StudentRepositoryTest 訂正済み

--- src/test/java/raisetech/student/management/Repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/student/management/Repository/StudentRepositoryTest.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import raisetech.student.management.date.Student;
 import raisetech.student.management.date.StudentCourse;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,14 +20,37 @@ class StudentRepositoryTest {
     @Test
     void 受講生の全件検索が行えること() {
         List<Student> actual = sut.search();
+
         assertThat(actual.size()).isEqualTo(5);
+
+        Student student = actual.get(0);
+        assertThat(student.getId()).isEqualTo("1");
+        assertThat(student.getName()).isEqualTo("山田太郎");
+        assertThat(student.getKanaName()).isEqualTo("ヤマダタロウ");
+        assertThat(student.getNickname()).isEqualTo("タロ");
+        assertThat(student.getEmail()).isEqualTo("taro@example.com");
+        assertThat(student.getArea()).isEqualTo("東京");
+        assertThat(student.getAge()).isEqualTo(25);
+        assertThat(student.getSex()).isEqualTo("男性");
     }
 
     @Test
     void 受講生IDで検索が行えること() {
         Student actual = sut.searchStudent("1");
-        assertThat(actual).isNotNull();
-        assertThat(actual.getId()).isEqualTo("1");
+
+        Student student = new Student();
+        student.setId("1");
+        student.setName("山田太郎");
+        student.setKanaName("ヤマダタロウ");
+        student.setNickname("タロ");
+        student.setEmail("taro@example.com");
+        student.setArea("東京");
+        student.setAge(25);
+        student.setSex("男性");
+        student.setRemark(null);
+        student.setDeleted(false);
+
+        assertThat(actual).isEqualTo(student);
     }
 
     @Test
@@ -44,17 +68,18 @@ class StudentRepositoryTest {
 
     @Test
     void 受講生の登録が行えること() {
-        Student student = new Student();
-        student.setName("山田太郎");
-        student.setKanaName("ヤマダタロウ");
-        student.setNickname("タロー");
-        student.setEmail("taro@example.com");
-        student.setArea("東京");
-        student.setAge(25);
-        student.setSex("男性");
-        student.setRemark("");
-        student.setDeleted(false);
-
+        Student student = new Student(
+                null,
+                "山田太郎",
+                "ヤマダタロウ",
+                "タロ",
+                "taro@example.com",
+                "東京",
+                25,
+                "男性",
+                "",
+                false
+        );
         sut.registerStudent(student);
 
         List<Student> actual = sut.search();
